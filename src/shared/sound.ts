8ba4dd966a8bb1a15c289f9728867fc1cf520569
Tiger Abrodi <tigerabrodi@gmail.com>
2026-03-30 00:02:50 +0800

    prettier

--- src/shared/sound.ts
+++ src/shared/sound.ts
@@ -1,7 +1,10 @@
 import { join } from 'path'
 
 const JUMP_SOUND_PATH = join(import.meta.dirname, 'retro-bounce-jump-sound.wav')
-const COLLECT_SOUND_PATH = join(import.meta.dirname, 'generic-collect-sound.wav')
+const COLLECT_SOUND_PATH = join(
+  import.meta.dirname,
+  'generic-collect-sound.wav'
+)
 
 export function playJumpSound(): void {
   Bun.spawn(['afplay', '-v', '0.5', JUMP_SOUND_PATH], {
